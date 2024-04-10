bf98b4cf40f74201dd93f502cfe04e2c0ff4a6d9
Kumar Priyanshu <kpriyanshu01programmer@gmail.com>
2024-04-10 17:10:07 +0530

    hehe

--- include/src/showMenu.c
+++ include/src/showMenu.c
@@ -4,8 +4,8 @@
 
 void showMenu()
 {
-    for (int i = 0; strcmp(Menu[i], "\0"); i++)
+    for(int i = 0; strcmp(Menu[i], "\0"); i++)
     {
-        printf("%d. %s\n", (i + 1), Menu[i]);
+        printf("%d. %s\n", (i+1), Menu[i]);
     }
 }

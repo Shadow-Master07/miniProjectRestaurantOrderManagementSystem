edf30fcf40e5a0ceb0a99f62c02a33be92951187
Kumar Priyanshu <kpriyanshu01programmer@gmail.com>
2024-04-11 01:40:59 +0530

    New thing, separated the files yet again

---
+++ include/lib/showOrdersAndStatus.h
@@ -0,0 +1,11 @@
+#ifndef SHOWORDERSANDSTATUS_H
+#define SHOWORDERSANDSTATUS_H
+
+#include "structDefinitions.h"
+#include "showMenu.h"
+
+void orderListStatus(orderList *orderHead);
+void showAllOrders(orderList *orderHead);
+
+
+#endif
---
+++ include/src/showOrdersAndStatus.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "../lib/showOrdersAndStatus.h"
+
+void orderListStatus(orderList *orderHead)
+{
+    int id;
+    printf("Enter order id\n");
+    scanf("%d", &id);
+    orderList *temp1 = orderHead;
+    if (temp1 == NULL)
+    {
+        printf("Not found");
+        return;
+    }
+    while (id != temp1->ID)
+    {
+        temp1 = temp1->next;
+    }
+    dishName *temp2 = temp1->dishHead;
+    while (temp2 != NULL)
+    {
+        printf("%s\n", Menu[temp2->dishNameNo]);
+        temp2 = temp2->next;
+    }
+}
+
+void showAllOrders(orderList *orderHead)
+{
+    orderList *temp1 = orderHead;
+    dishName *temp2 = NULL;
+    int i;
+    printf("Printing Order ID:\n");
+    while (temp1 != NULL)
+    {
+        printf("%d:\n", temp1->ID);
+        temp2 = temp1->dishHead;
+        i = 1;
+        while (temp2 != NULL)
+        {
+            printf("    %d. %s\n", i, Menu[temp2->dishNameNo]);
+            temp2 = temp2->next;
+            i++;
+        }
+        temp1 = temp1->next;
+    }
+    printf("Order list completed\n");
+}
